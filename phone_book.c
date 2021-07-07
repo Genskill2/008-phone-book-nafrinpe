121394165a20743e4763fba6f9115e3ddd54524f
nafrinpe <84736105+nafrinpe@users.noreply.github.com>
2021-07-07 16:34:14 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -1,4 +1,5 @@
-include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 const char *DB="directory.db";
@@ -11,21 +12,12 @@ struct entry0 {
 
 typedef struct entry0 entry;
 
-/* Command handlers */
 void add(char *, char *);
 int search(FILE *,char *);
 void list(FILE *);
 int delete(FILE *, char *);
 
-/* Utility functions  */
-FILE * open_db_file(); /* Opens the database file. Prints error and
-                          quits if it's not available */
-void print_usage(char *, char *);  /* Prints usage */
-entry *load_entries(FILE *);         /* Load all entries from the
-                                      database file. Returns pointer
-                                      to first entry */
-entry *create_entry_node(char *, char *);  /* Create a new entry
-                                              node. Has to be 
+FILE * open_db_file();
 void print_usage(char *, char *);
 entry *load_entries(FILE *);
 entry*create_entry_node(char *, char *);
@@ -55,9 +47,7 @@ int main(int argc, char *argv[]) {
     list(fp);
     fclose(fp);
     exit(0);
-  } 
-else if (strcmp(argv[1], "search") == 0)
- {
+  } else if (strcmp(argv[1], "search") == 0) {
 if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
       exit(1);
@@ -225,3 +215,4 @@ if(p==base){
   free_entries(base);
   return deleted;
 }
+
